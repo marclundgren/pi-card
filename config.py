5b4964bb139f1ca1994e636fe6f5629af63fdbb0
Marc Lundgren <marcrlundgren@gmail.com>
2024-06-03 19:16:44 -0700

    --wip-- [skip ci]

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 config = {
     "SOUNDS_PATH": 'sounds/',
-    "WAKE_WORD": ["test", "johhny", "five", "wake"],
+    "WAKE_WORD": ["rasp", "berry", "barry", "razbear", "brad", "raster", "test", "wake"],
     "TIMEOUT": 10,
     # longest amount of time the allow a phrase to continue before stopping the recording
     "PHRASE_TIME_LIMIT": 7,
     "USE_FASTER_WHISPER": False,
     "WHISPER_CPP_PATH": "../whisper.cpp/",
-    "WHISPER_MODEL_PATH": "../whisper.cpp/models/ggml-tin.bin",
+    "WHISPER_MODEL_PATH": "../whisper.cpp/models/ggml-tiny.bin",
     "LLAMA_CPP_PATH": "../llama.cpp/",
     # "MOONDREAM_MMPROJ_PATH": "../moondream-quants/moondream2-mmproj-050824-f16.gguf",
     # "MOONDREAM_MODEL_PATH": "../moondream-quants/moondream2-050824-q8.gguf",
@@ -16,7 +16,11 @@ config = {
     "CONDENSE_MESSAGES": True,  # for faster response time
     # number of messages to keep in memory (odd #s work best)
     "TRAILING_MESSAGE_COUNT": 1,
-    "SYSTEM_PROMPT": 'You are Johnny Five, a Raspbery Pi Voice Assistant. Answer questions in only a sentence.',
+    "SYSTEM_PROMPT": 'You are Pi-Card, a Raspbery Pi Voice Assistant. Answer questions in only a sentence.',
     "SPEECH_VOLUME": 10, # 1 -> 100,
     "START_WITH_MIC_CHECK": True, # if True, will start with a check to see if the microphone is working
+    "START_WITH_TRANSCRIPTION_TEST": False, # if True, will start with a test to see if the model is working,
+    "LOG_FILE_PATH": "./logs/log.txt", # path to log file,
+    "DEVICE_INDEX_MIC": 0, # index of the microphone device,
+    "OLLAMA_CHAT_STREAM": True, # if True, will use the ollama chat stream
 }
